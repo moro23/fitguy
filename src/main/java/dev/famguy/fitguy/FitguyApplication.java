4cfeb6dbdd76ad0cbe9fbd700ec577f6fac43a3b
moro23 <billhab66@gmail.com>
2025-07-27 08:04:20 +0000

    project startup files.

---
+++ src/main/java/dev/famguy/fitguy/FitguyApplication.java
@@ -0,0 +1,13 @@
+package dev.famguy.fitguy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitguyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitguyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/famguy/fitguy/FitguyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.famguy.fitguy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitguyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
